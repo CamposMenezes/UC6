f0f3d7be2f3b74027b00f85d33e19012578a85b8
Vinicius Menezes <vini.menezescampos@gmail.com>
2025-10-06 22:10:40 -0300

    Aula 01

---
+++ 01/NovoEstacionamento/NovoEstacionamento/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovoEstacionamento.Models
+{
+    internal class Cliente
+    {
+
+        public int ID { get; set; }
+        public string Nome { get; set; }
+
+        public Cliente(string nome)
+        {
+            Nome = nome;
+        }
+
+        public string cpf { get; set; }
+        public string Telefone { get; set; }
+        public string email { get; set; }
+
+    }
+}
---
+++ 01/NovoEstacionamento/NovoEstacionamento/Program.cs
@@ -0,0 +1,5 @@
+﻿using NovoEstacionamento.Models;
+
+var c1 = new Cliente("João Silva");
+Console.WriteLine(c1.Nome);
+
