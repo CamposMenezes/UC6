9e8c567bd68320d9c211d631db37c50803e59559
Vinicius Menezes <vini.menezescampos@gmail.com>
2025-11-17 19:39:04 -0300

    Projeto desktop

---
+++ 05/EstacionamentoSenac/Estacionamento.MAUI/Models/Veiculo.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Estacionamento.MAUI.Models
+{
+    internal class Veiculo
+    {
+        public int id { get; set; }
+        public string placa { get; set; }
+        public string? Modelo { get; set; }
+        public string? Cor { get; set; }
+        public int MotoristaId { get; set; }
+
+        public Motorista Motorista { get; set; }
+
+    }
+}
